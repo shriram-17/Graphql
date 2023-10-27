67fcf7be14afd2515b28ebb0bf542fd237f791de
shriram-17 <76253109+shriram-17@users.noreply.github.com>
2023-10-27 18:42:35 +0530

    add the ts code for server

--- server/typescript/index.ts
+++ server/typescript/index.ts
@@ -1,20 +1,35 @@
 import fastify from "fastify";
 import { AppDataSource } from "./src/data-source";
+import authorRouter from "./src/routes/authors";
+import bookRouter from "./src/routes/books";
+import { ApolloServer, gql } from "apollo-server-fastify";
+import fs from 'fs';
+import { resolvers } from "./src/graphql-resolvers";
+
+
+const typeDefs = gql(fs.readFileSync("./schema.graphql", { encoding: "utf8" }));
 
 const app = fastify({ logger: true });
 
-app.get("/", async (request, reply) => {
-  reply.send({ "Message": "Hello World" });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
-
 
 AppDataSource.initialize()
   .then(() => {
     console.log("Postgres is now Connected")
   })
-  .catch((err) => {
+  .catch((err : any) => {
     console.error("Error during Data Source initialization", err)
-  })
+  });
+
+app.register(authorRouter,{prefix:"/author"});
+app.register(bookRouter,{prefix:"/book"});
+
+app.get("/", async (request :any , reply: any) => {
+  reply.send({ "Message": "Hello World" });
+});
 
 const start = async () => {
   try {
