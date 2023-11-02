d32d2e57dd457b7404f6de9eba0a16327f6f57c3
shriram-17 <76253109+shriram-17@users.noreply.github.com>
2023-11-02 19:37:32 +0530

    add the two mutation query

--- server/typescript/src/graphql-resolvers.ts
+++ server/typescript/src/graphql-resolvers.ts
@@ -1,7 +1,6 @@
 import { AppDataSource } from "./data-source";
 import { Author } from "./entity/author";
 import { Book } from "./entity/book";
-import { GetAuthorWithBooksArgs } from './types';
 
 export const resolvers = {
   Query: {
@@ -71,5 +70,86 @@ export const resolvers = {
         throw new Error("Failed to fetch author with books");
       }
     }
+  },
+  Mutation :{
+   createAuthor: async(_:unknown ,{name}: {name:string} ) : Promise<Author | string> => {
+      try {
+        const newAuthor = await AppDataSource.getRepository(Author).save({
+          name:name
+        });
+        console.log("Created author:", newAuthor);
+        return newAuthor;
+
+      } catch (error) {
+        console.error("Error creating author:", error);
+        throw new Error("Failed to create author");
+      }
+   },
+   createBook : async (_: unknown ,{title , authorId} :{ title : string, authorId : number}) : Promise<Book| string> => {
+    try {
+      console.log()
+      console.log("author_id",authorId);
+      const newAuthor  = await AppDataSource.getRepository(Author).findOne({
+        where:{
+          id : authorId
+        }
+      });
+
+      if(newAuthor)
+      {
+        const newBook = new Book();
+        newBook.title = title;
+        newBook.author = newAuthor;
+        const saved_book = await AppDataSource.getRepository(Book).save(newBook);
+        return  saved_book;
+      }
+      
+      throw new Error("Failed to find author_id")
+    } catch (error) {
+      console.error("Error creating book:", error);
+      throw new Error("Failed to create book");
+    }
+   },
+   deleteAuthor: async (_: unknown, { id }: { id: number }): Promise<string> => {
+    try {
+      const authorToDelete = await AppDataSource.getRepository(Author).findOne({
+        where: {
+          id: id,
+        },
+      });
+
+      if (authorToDelete) {
+        await AppDataSource.getRepository(Author).remove(authorToDelete);
+        console.log("Deleted author:", authorToDelete);
+        return `Author with ID ${id} has been deleted`;
+      } else {
+        throw new Error("Author not found");
+      }
+    } catch (error) {
+      console.error("Error deleting author:", error);
+      throw new Error("Failed to delete author");
+    }
+  },
+
+  deleteBook: async (_: unknown, { bid }: { bid: number }): Promise<string> => {
+    try {
+      const bookToDelete = await AppDataSource.getRepository(Book).findOne({
+        where: {
+          bid: bid,
+        },
+      });
+
+      if (bookToDelete) {
+        await AppDataSource.getRepository(Book).remove(bookToDelete);
+        console.log("Deleted book:", bookToDelete);
+        return `Book with bid ${bid} has been deleted`;
+      } else {
+        throw new Error("Book not found");
+      }
+    } catch (error) {
+      console.error("Error deleting book:", error);
+      throw new Error("Failed to delete book");
+    }
+  } 
   }   
 };
